6501b3b2d567409959e8cbed9a32ec8e3a8adb2c
Julio Borges <130866392+julioborgesss@users.noreply.github.com>
2023-08-24 09:55:36 -0300

    Update RJJsql.sql

--- RJJsql.sql
+++ RJJsql.sql
@@ -25,7 +25,27 @@ create table usuario(
  foreign key (idUser) references usuario(id),
  foreign key (idPessoa) references pessoa(id)
  );
- 
+ create table Ongs(
+ id int primary key auto_increment,
+ cnpj int not null,
+ nomeSocial int not null,
+ RazaoSocial varchar(100)
+ );
+ create table triagem(
+ id int primary key auto_increment,
+ tipo varchar(150) not null,
+ idOngs int not null,
+ foreign key (idOngs) references Ongs(id)
+ );
+ create table pessoa_triagem(
+ id int primary key auto_increment,
+ idPessoa int not null,
+ idTriagem int not null,
+ foreign key (idPessoa) references pessoa(id),
+ foreign key (idTriagem) references triagem(id)
+ );
+ alter table pessoa
+ add column nome varchar(100) not null;
  
  
  
